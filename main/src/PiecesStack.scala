fd2843a7371b782f1f6b251aa2c06dc80431f53a
egor <kokkoe5@aalto.fi>
2023-04-03 13:25:31 +0300

    new methods in PiecesStack and File class for the operations in file

--- main/src/PiecesStack.scala
+++ main/src/PiecesStack.scala
@@ -16,4 +16,51 @@ class PiecesStack {
 
   def pieceOnIndex(i: Int): Option[Piece] = if (this.size - 1 >= i) Some(this.stack(i)) else None
 
+  def addPiece(p: Piece) = {
+    if (!this.stack.exists( _.samePiece(p) )) {
+      this.stack += p
+      this.current = Some(p)
+    }
+  }
+
+  def takePiece(p: Piece) = {
+    if (this.stack.exists( _.samePiece(p) )) {
+      if (p.samePiece(this.current.getOrElse(new Piece('x', 'x', 'x', 1)))) {
+        if (this.size == 1) {
+          this.current = None
+        } else {
+          this.flipRight()
+        }
+      }
+      this.stack -= p
+    }
+  }
+
+
+  def flipLeft() = {
+    if (this.size > 1) {
+      val i = this.stack.indexWhere( _.samePiece(this.current.get) )
+      if (i == 0) {
+        this.current = Some(this.stack(this.size - 1))
+      } else this.current = Some(this.stack(i - 1))
+    }
+  }
+
+
+  def flipRight() = {
+    if (this.size > 1) {
+      val i = this.stack.indexWhere( _.samePiece(this.current.get))
+      if (i == this.size - 1) {
+        this.current = Some(this.stack.head)
+      } else this.current = Some(this.stack(i + 1))
+    }
+  }
+
+  def empty() = {
+    if (!this.isEmpty) {
+      for (i <- 0 until this.size) {
+        this.takePiece(stack.head)
+      }
+    }
+  }
 }
